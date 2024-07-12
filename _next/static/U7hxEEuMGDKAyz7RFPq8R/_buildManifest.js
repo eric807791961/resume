d2e41a19d0db813e65d116abeee5f2fa1e60b87f
kwhuang <erichuang8077@gmail.com>
2024-07-13 00:52:04 +0800

    Updates

--- _next/static/U7hxEEuMGDKAyz7RFPq8R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/533-c63fa819de3f3699.js","static/chunks/pages/index-bc441c61f0307ecd.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
