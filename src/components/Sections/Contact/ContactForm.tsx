d1e0cf97263101eaf0d74876a31d50479b7e8506
kwhuang <erichuang8077@gmail.com>
2024-07-16 06:52:15 +0800

    finish mailing

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
+import { FC, memo, useCallback, useMemo, useState } from 'react';
 
 interface FormData {
   name: string;
@@ -20,9 +20,9 @@ const ContactForm: FC = memo(() => {
 
   const onChange = useCallback(
     <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
-      const fieldData: Partial<FormData> = {[name]: value};
-      setData((prevData) => ({...prevData, ...fieldData}));
+      const { name, value } = event.target;
+      const fieldData: Partial<FormData> = { [name]: value };
+      setData((prevData) => ({ ...prevData, ...fieldData }));
     },
     [],
   );
--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,33 +1,34 @@
-import {NextApiRequest, NextApiResponse} from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.GMAIL_USER, // Your Gmail address
-    pass: process.env.GMAIL_PASS, // Your Gmail password or App password
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
   },
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const {name, email, message} = req.body;
+    const { name, email, message } = req.body;
 
     const mailOptions = {
       from: email,
-      to: process.env.RECIPIENT_EMAIL, // Your recipient email address
+      to: process.env.RECIPIENT_EMAIL,
       subject: `Contact form submission from ${name}`,
       text: message,
+      html: `<p>${message}</p>`,
     };
 
     try {
       await transporter.sendMail(mailOptions);
-      res.status(200).json({success: true});
+      res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
       console.error('Error sending email:', error);
-      res.status(500).json({error: 'Failed to send email'});
+      res.status(500).json({ message: 'Failed to send email' });
     }
   } else {
-    res.status(405).json({error: 'Method not allowed'});
+    res.status(405).json({ message: 'Method not allowed' });
   }
 };
